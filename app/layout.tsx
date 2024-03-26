3c85bf400e64bd61cc9eee5fe88c7378a4b897ce
Jilo <116283418+jeanloick@users.noreply.github.com>
2024-03-26 14:20:16 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import { Analytics } from "./components/analytics";
 
 export const metadata: Metadata = {
   title: {
-    default: "chronark.com",
-    template: "%s | chronark.com",
+    default: "DevCrypo.com",
+    template: "%s | DevCrypto.com",
   },
   description: "Co-founder of unkey.dev and founder of planetfall.io",
   openGraph: {
